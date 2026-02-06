38922182791edadd6c960e9b1f3b3ca8b75a915b
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-02-06 08:28:09 +0000

    Auto-backup: 2/6/2026, 8:28:09 AM - Updated project/app.js

--- project/app.js
+++ project/app.js
@@ -1,3 +1,4 @@
 const btn = document.querySelectorAll('button')
-console.log(btn[0].classList.value)
-console.log(btn[0].classList)
+//console.log(btn[0].classList.value)
+//console.log(btn[0].classList)
+console.log(btn[0].classList.add('yellow'))
