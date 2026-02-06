28ee6ad1a0ba54c7fe84b430cc8dddc5c9f81213
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-02-06 09:01:07 +0000

    Auto-backup: 2/6/2026, 9:01:07 AM - Updated project/app.js

--- project/app.js
+++ project/app.js
@@ -5,6 +5,13 @@ const btn = document.querySelectorAll('button')
 //console.log(btn[0].classList.remove('blue')) //removing class
 //console.log(btn[0].classList.toggle('blue')) //adds class if there is no, or removes
 
-if (btn[0].classList.contains('red')) {
+/* if (btn[0].classList.contains('red')) {
 	console.log('Element contains blue class')
-}
+} */
+
+btn[0].addEventListener('click', () => {
+	if (btn[0].classList.contains('red')) {
+		console.log('Element contains red class')
+	} else {
+	}
+})
