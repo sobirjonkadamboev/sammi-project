8603ee70987537da11be0f4341beedc85fab7e25
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-02-06 08:36:59 +0000

    Auto-backup: 2/6/2026, 8:36:59 AM - Updated project/app.js

--- project/app.js
+++ project/app.js
@@ -1,6 +1,8 @@
 const btn = document.querySelectorAll('button')
 //console.log(btn[0].classList.value)
 //console.log(btn[0].classList)
-console.log(btn[0].classList.add('yellow')) //adding class
+//console.log(btn[0].classList.add('yellow')) //adding class
 //console.log(btn[0].classList.remove('blue')) //removing class
-console.log(btn[0].classList.toggle('blue')) //adds class if there is no, or removes
+//console.log(btn[0].classList.toggle('blue')) //adds class if there is no, or removes
+
+if(btn[0].classList.contains('blue')) 
