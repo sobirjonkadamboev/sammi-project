255793bebdf589036ed9ce8944f2119013b41ca3
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-02-08 13:30:53 +0000

    Auto-backup: 2/8/2026, 1:30:53 PM - Updated project/script.js

--- project/script.js
+++ project/script.js
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	function showTabContent(index = 0) {
 		tabsContent[index].style.display = 'flex'
-		tabs[index].classList.add('tabheader__item tabheader__item_active')
+		tabs[index].classList.add('tabheader__item_active')
 	}
 	hideContent()
 	showTabContent()
