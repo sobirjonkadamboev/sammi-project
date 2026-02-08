4f0b4e1a653b11e4e1594903f65c0551f05e2aaa
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-02-08 13:35:32 +0000

    Auto-backup: 2/8/2026, 1:35:32 PM - Updated project/script.js

--- project/script.js
+++ project/script.js
@@ -3,12 +3,6 @@ window.addEventListener('DOMContentLoaded', () => {
 		tabsContent = document.querySelectorAll('.tab_content'),
 		tabsParents = document.querySelector('.tabheader__items')
 
-	function hideContent() {
-		tabsContent.forEach(tabsContent => {
-			tabsContent.style.display = 'none'
-		})
-	}
-
 	tabs.forEach(tab => {
 		tab.classList.remove('tabheader__item_active')
 	})
