2289507c7fc75f03b103c246dda0edda575ec0a6
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-02-09 12:26:53 +0000

    Auto-backup: 2/9/2026, 12:26:53 PM - Updated project/interval-project.js

--- project/interval-project.js
+++ project/interval-project.js
@@ -39,11 +39,13 @@ let id = setTimeout(function log() {
 	id = setTimeout(log, 500)
 }, 500) */
 
+const btn = document.querySelector('#btn')
 const moveCar = () => {
 	const car = document.querySelector('.car')
 	let position = 0
 
 	const animationId = setInterval(moving, 10)
+
 	function moving() {
 		if (position === 1000) {
 			clearInterval()
