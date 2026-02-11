0eae15eb4bd5669b2ac2b87c0b8d0abb2a11a8d0
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-02-11 09:15:26 +0000

    Auto-backup: 2/11/2026, 9:15:26 AM - Updated project/date.js

--- project/date.js
+++ project/date.js
@@ -1,10 +1,17 @@
-const now = new Date()
+// const now = new Date()
 
-const parse = Date.parse('2006-11-15')
-console.log(parse)
-// console.log(now.getFullYear())
-// console.log(now.getHours())
-// console.log(now.getUTCHours())
+// const parse = Date.parse('2006-11-15')
+// console.log(parse)
+// // console.log(now.getFullYear())
+// // console.log(now.getHours())
+// // console.log(now.getUTCHours())
 
-// now.setHours(12)
-// console.log(now)
+// // now.setHours(12)
+// // console.log(now)
+
+
+let startDate = new Date()
+
+for(let i = 0; i < 100; i++){
+	let number i ** 3
+}
